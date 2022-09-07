a6b134d37a5a1129ff766bc28ab8357b3b390f3d
Nick Cesare <ncesare@gmail.com>
2022-09-07 08:38:34 -0600

    Wrote for loops to print out a grid.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const container = document.querySelector('.container')
+const sketchGrid = document.querySelector('.sketch-grid');
+
+//Needs slider to determine n and i values
+let gridSize = 16;
+
+for (let n = 0; n < gridSize; n++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    sketchGrid.appendChild(row);
+    console.log(`${n} row added`);
+    for (let i = 0; i < gridSize; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        row.appendChild(box);
+        console.log(`${n} box added`);
+    }
+}
+
